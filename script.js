66d7b46e14cd967623ae6d874f088069d49f505c
skiddled <66100859+skiddled@users.noreply.github.com>
2024-04-09 19:05:56 -0500

    attempting fix

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const flickityElem = document.querySelector('.slider');
 
 if (flickityElem) {
   const flickity = new Flickity(flickityElem, {
-    cellAlign: 'left', autoPlay: 5000, wrapAround: true, width: 100%,
+    cellAlign: 'left', autoPlay: 5000, wrapAround: true, width: 100%
   });
 }
